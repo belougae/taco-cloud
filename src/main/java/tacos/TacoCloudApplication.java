6ac20957a5313be27b9a2a694a093c3c0c1f6428
Belouga.E <wangjingnic1993@gmail.com>
2021-07-07 16:41:29 +0800

    Init

---
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 组合注解
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeControllerTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
